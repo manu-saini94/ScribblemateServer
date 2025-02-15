d4c66718a38d47e27f32b04f2994d45ef20a19eb
manu-saini94 <manu.saini94@gmail.com>
2025-02-15 21:41:42 +0530

    authentication filter fixes for public endpoints

--- src/main/java/com/scribblemate/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/scribblemate/configuration/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.scribblemate.configuration;
 
 import java.io.IOException;
+
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -8,6 +9,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -23,67 +27,61 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private final HandlerExceptionResolver handlerExceptionResolver;
+    private final HandlerExceptionResolver handlerExceptionResolver;
+    private final JwtAuthenticationService jwtService;
+    private final UserDetailsService userDetailsService;
 
-	private final JwtAuthenticationService jwtService;
-	private final UserDetailsService userDetailsService;
+    public JwtAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver,
+                                   JwtAuthenticationService jwtService, UserDetailsService userDetailsService) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
-	public JwtAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver,
-			JwtAuthenticationService jwtService, UserDetailsService userDetailsService) {
-		this.handlerExceptionResolver = handlerExceptionResolver;
-		this.jwtService = jwtService;
-		this.userDetailsService = userDetailsService;
-	}
+    private final RequestMatcher skipMatcher = new OrRequestMatcher(
+            new AntPathRequestMatcher("/api/v1/auth/signup"),
+            new AntPathRequestMatcher("/api/v1/auth/forgot"),
+            new AntPathRequestMatcher("/api/v1/auth/login")
+    );
 
-	@Override
-	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
-			@NonNull FilterChain filterChain) throws ServletException, IOException {
-		// Extract accessToken from cookies
-		Cookie[] cookies = request.getCookies();
-		String jwt = null;
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return skipMatcher.matches(request);
+    }
 
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if ("accessToken".equals(cookie.getName())) {
-					jwt = cookie.getValue();
-					break;
-				}
-			}
-		}
-
-		// If accessToken is missing, proceed with the filter chain
-		if (jwt == null) {
-			filterChain.doFilter(request, response);
-			return;
-		}
-
-		try {
-			// Extract the user email from the JWT token
-			String userEmail = jwtService.extractUsername(jwt);
-
-			// Check if the user is already authenticated
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-			if (userEmail != null && authentication == null) {
-				UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-
-				// Validate the JWT token
-				if (jwtService.isTokenValid(jwt, userDetails)) {
-					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
-							null, userDetails.getAuthorities());
-
-					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-					SecurityContextHolder.getContext().setAuthentication(authToken);
-				}
-			}
-
-			// Continue with the filter chain
-			filterChain.doFilter(request, response);
-
-		} catch (Exception exception) {
-			// Handle any exceptions that occur during authentication
-			handlerExceptionResolver.resolveException(request, response, null, exception);
-		}
-	}
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        String jwt = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("accessToken".equals(cookie.getName())) {
+                    jwt = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if (jwt == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        try {
+            String userEmail = jwtService.extractUsername(jwt);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (userEmail != null && authentication == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                            null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception exception) {
+            handlerExceptionResolver.resolveException(request, response, null, exception);
+        }
+    }
 
 }
